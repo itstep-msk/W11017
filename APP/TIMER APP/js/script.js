f554b74716b434b9ac52d6667ff8ad72a53c5473
space2pacman <space2pacman@gmail.com>
2018-09-28 22:10:31 +0300

    add 18.09.28

--- APP/TIMER APP/js/script.js
+++ APP/TIMER APP/js/script.js
@@ -1,23 +1,64 @@
-var minutes = $(".minutes");
-var circle = $('.circle');
-var propeller = new Propeller(circle);
+var el = {
+	minutes: $(".minutes"),
+	hours: $(".hours"),
+	circle: $(".circle"),
+	numbers: $(".numbers")
+}
+var counts = {
+	minutes: 0,
+	hours: 0
+}
+var incHours = 0;
+var lastAngle = 0;
+var access = true;
+var propeller = new Propeller(el.circle);
 var step = 6;
-var i;
+var timer;
 
+el.minutes.html(counts.minutes);
+el.hours.html(counts.hours);
+
+// При вращении передаются цифры на элемент
 propeller.onRotate = function() {
-	minutes.html(Math.floor(this.angle / step))
+	var angleToMinutes = Math.floor(this.angle / step);
+
+	checkHour(angleToMinutes);
+	el.minutes.html(angleToMinutes)
+}
+
+function checkHour(min) {
+	if(min == 59 && lastAngle == 58) {
+		incHours++;
+		setHour();
+	} else if(min == 59 && lastAngle == 0){
+		if(incHours > 0) {
+			incHours--;
+		}
+		
+		setHour();
+	}
+
+	lastAngle = min;
+}
+
+function setHour() {
+	el.hours.html(incHours)
 }
 
 function startTimer() {
-	if(propeller.angle <= step) {
+	if(propeller.angle <= step && incHours == 0) {
 		propeller.angle = step;
-		clearInterval(i)
+		clearInterval(timer)
+		access = true;
 	}
 	
 	propeller.angle -= step;
-	minutes.html(Math.floor(propeller.angle / step))
+	el.minutes.html(Math.floor(propeller.angle / step))
 }
 
-minutes.on("click", function() {
-	i = setInterval(startTimer, 1000)
+el.numbers.on("click", function() {
+	if(access) {
+		timer = setInterval(startTimer, 10)
+		access = false;
+	}
 })
